0504ac3339a096901ca51481d1b18f0a2614385b
DiegoSarti <diegovianasa14@gmail.com>
2024-11-11 20:14:46 -0300

    exercicio de for

---
+++ atividade-for/for.js
@@ -0,0 +1,59 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// ------------------------------------------------
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+// -------------------------------------------------
+// let soma = 0;
+// for (let i = 1; i <= 100; i++) {
+//     soma += i;
+// }
+// console.log(soma);
+// -----------------------------------------------
+
+// let frutas = ['maçã', 'banana', 'laranja', 'morango', 'uva'];
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+// --------------------------------------------------
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * 2);
+// }
+// ----------------------------------------------------
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// ----------------------------------------------------
+// let numeros = [3, 5, 7, 2, 8, 10, 1];
+// let maior = numeros[0];
+// for (let i = 1; i < numeros.length; i++) {
+//     if (numeros[i] > maior) {
+//         maior = numeros[i];
+//     }
+// }
+// console.log(maior);
+// -----------------------------------------------------
+// let contador = 0;
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 !== 0) {
+//         contador++;
+//     }
+// }
+// console.log(contador);
+// ------------------------------------------------------
+// let numero = 7; 
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${numero} x ${i} = ${numero * i}`);
+// }
+// -------------------------------------------------------
+// let str = 'exemplo';
+// let reversa = '';
+// for (let i = str.length - 1; i >= 0; i--) {
+//     reversa += str[i];
+// }
+// console.log(reversa);
+// ------------------------------------------------------
