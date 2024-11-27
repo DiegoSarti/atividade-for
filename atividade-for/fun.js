816b249055346da19f465bfa354bef1e2821e66f
DiegoSarti <diegovianasa14@gmail.com>
2024-11-27 20:02:32 -0300

    exercicio split e pegar o ultimo nome do lenght

--- atividade-for/fun.js
+++ atividade-for/fun.js
@@ -1,6 +1,6 @@
 // metodo lenght length conta caracter
 
-const fullname = 'Lucas do Espirito Santo'
+const fullname = 'Lucas do Espirito Santos'
 
 console.log(fullname.length, 'conta caracteres')
 
@@ -9,5 +9,13 @@ console.log(fruits.length, 'array')
 console.table(fruits)//faz uma tabela visivel
 console.log(fruits[fruits.length -1])
 
-const cortaNome = fullname.slice(1,5)
+const cortaNome = fullname.slice(1,5)//slice determina que vai do primeiro numro que voce decicir até o segundo
 console.log(cortaNome)
+
+const transformToArray = fullname.split(' ')
+console.log(transformToArray)
+console.log(transformToArray[transformToArray.length -1]) 
+console.table(transformToArray)
+
+const lastName = transformToArray[transformToArray.length -1]
+console.log(lastName)
