d950976e386d78762ee007beffe6ecc235a63d91
DiegoSarti <diegovianasa14@gmail.com>
2024-11-27 19:33:49 -0300

    exercicio de lenght

---
+++ atividade-for/fun.js
@@ -0,0 +1,10 @@
+// metodo lenght length conta caracter
+
+const fullname = 'Lucas do Espirito Santo'
+
+console.log(fullname.length, 'conta caracteres')
+
+const fruits = ['maça','pessego','uva','pera']
+console.log(fruits.length, 'array')
+console.table(fruits)
+console.log(fruits[fruits.length -1])
