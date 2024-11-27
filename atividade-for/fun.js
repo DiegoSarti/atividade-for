6ccf94aa745729cda335cd041970183755aeaced
DiegoSarti <diegovianasa14@gmail.com>
2024-11-27 19:42:35 -0300

    exercicios de slice e table e slice

--- atividade-for/fun.js
+++ atividade-for/fun.js
@@ -6,5 +6,8 @@ console.log(fullname.length, 'conta caracteres')
 
 const fruits = ['maça','pessego','uva','pera']
 console.log(fruits.length, 'array')
-console.table(fruits)
+console.table(fruits)//faz uma tabela visivel
 console.log(fruits[fruits.length -1])
+
+const cortaNome = fullname.slice(1,5)
+console.log(cortaNome)
